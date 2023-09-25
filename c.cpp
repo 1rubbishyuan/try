9eeeae7739f8d60633381154956ec3d7cc28e19a
1rubbishyuan <2773496952@qq.com>
2023-09-25 20:55:45 +0800

    modify c.cpp

--- c.cpp
+++ c.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
 using namespace std;
+unsigned u(unsigned b ){
+return b/8;
+}
 int a[2][1000000];
 int main(){
+short m=-32768;
+if(m==-(-m)){
 cout<<1;
+}
+else{
+cout<<2;}
+
 return 0;
 
 
