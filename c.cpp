1f0e034eb735f952101807f1b18514608cec647b
1rubbishyuan <2773496952@qq.com>
2023-09-25 20:56:57 +0800

    modify c.cpp twice

--- c.cpp
+++ c.cpp
@@ -1,17 +1,8 @@
 #include<iostream>
 using namespace std;
-unsigned u(unsigned b ){
-return b/8;
-}
-int a[2][1000000];
+int a[2][100000000];
 int main(){
-short m=-32768;
-if(m==-(-m)){
 cout<<1;
-}
-else{
-cout<<2;}
-
 return 0;
 
 
